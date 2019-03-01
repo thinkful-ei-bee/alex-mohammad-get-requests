b3b213cd18b3219f603e9ada7e9076c97cf8102f
Alex Varghese <alex.varghese250@gmail.com>
2019-03-01 12:45:36 -0500

    fixed the console lof for part 1 of the project

--- randomDogs/index.js
+++ randomDogs/index.js
@@ -3,11 +3,11 @@
 function getDogImage(newNumber) {
   fetch(`https://dog.ceo/api/breeds/image/random/${newNumber}`)
     .then(response => response.json())
-    //.then(responseJson => console.log(responseJson));
     .then(responseJson => displayPictures(responseJson, newNumber));
 }
 
 function displayPictures(responseJson, newNumber) {
+  console.log(responseJson);
   $('.pictures').html('');
   for (let i = 0; i < newNumber; i++) {
     $('.pictures').append(`<img src="${responseJson.message[i]}" class="picture">`);
